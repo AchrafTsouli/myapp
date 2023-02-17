489ed1ea3f87a254023a42556e0717abde7def65
ACHRAF TSOULI <ACHRAF.TSOULI-ETU@etu.univh2c.ma>
2023-02-17 20:26:09 +0100

    changes

--- e-commerce/lib/main.dart
+++ e-commerce/lib/main.dart
@@ -3,6 +3,7 @@ import 'screen/auth/onboardingScreen.dart';
 import 'screen/auth/signin.dart';
 import 'screen/auth/signup.dart';
 import 'screen/home/home_screen.dart';
+import 'screen/profile_screen/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ecommerce',
       theme: ThemeData(
+        fontFamily: 'Muli',
         primarySwatch: Colors.grey,
       ),
       //home: const Signup(),
-      initialRoute: "/onboard",
+      initialRoute: "/profile",
       routes: {
         "/signin": (context) => const Signin(),
         "/signup": (context) => const Signup(),
         "/home": (context) => const HomeScreen(),
         "/onboard": (context) => const OnboardingScreen(),
+        "/profile": (context) => const ProfileScreen(),
       },
     );
   }
--- e-commerce/lib/screen/profile_screen/profile.dart
+++ e-commerce/lib/screen/profile_screen/profile.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'components/profile_image.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(),
+        body: Padding(
+      padding: EdgeInsets.all(35),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                Text("Profile",
+                    style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey)),
+                SizedBox(
+                  width: 20,
+                )
+              ],
+            ),
+            SizedBox(height: 20),
+            ProfilePicture(),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
